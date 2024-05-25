40838c613f74e68d0e12dd38bfb4d58f593a359e
Roberto Almeida <univil@gmail.com>
2024-05-25 17:26:04 -0500

    Change price to type Currency

--- models/car.js
+++ models/car.js
@@ -1,34 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const carSchema = new Schema({
+require("mongoose-currency").loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const carSchema = new Schema(
+  {
     make: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     model: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     color: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     price: {
-        type: String,
-        required: true
+      type: Currency,
+      required: true,
+      min: 0,
     },
     vin: {
-        type: String,
-        required: true,
-        unique: true
-    }
-}, {
-    timestamps: true
-});
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-const Car = mongoose.model('Car', carSchema);
+const Car = mongoose.model("Car", carSchema);
 
 module.exports = Car;
-
